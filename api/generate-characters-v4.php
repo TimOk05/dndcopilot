1a2dc4b1e52eba6ec7cdfe27084d3a80c3d56a78
Timofey <timuamua@gmail.com>
2025-09-01 18:32:32 +0300

    up

--- api/generate-characters-v4.php
+++ api/generate-characters-v4.php
@@ -10,6 +10,50 @@ class CharacterGeneratorV4 {
     private $occupations = [];
     private $race_names = [];
     
+    // Русские названия рас
+    private $race_translations = [
+        'aarakocra' => 'Ааракокра',
+        'aasimar' => 'Аасимар',
+        'bugbear' => 'Багбир',
+        'dragonborn' => 'Драконорожденный',
+        'dwarf' => 'Дварф',
+        'elf' => 'Эльф',
+        'firbolg' => 'Фирболг',
+        'genasi' => 'Генаси',
+        'gnome' => 'Гном',
+        'goblin' => 'Гоблин',
+        'goliath' => 'Голиаф',
+        'half-elf' => 'Полуэльф',
+        'half-orc' => 'Полуорк',
+        'halfling' => 'Полурослик',
+        'human' => 'Человек',
+        'kenku' => 'Кенку',
+        'kobold' => 'Кобольд',
+        'lizardfolk' => 'Людоящер',
+        'orc' => 'Орк',
+        'tabaxi' => 'Табакси',
+        'tiefling' => 'Тифлинг',
+        'triton' => 'Тритон',
+        'yuan-ti' => 'Юань-ти'
+    ];
+    
+    // Русские названия классов
+    private $class_translations = [
+        'barbarian' => 'Варвар',
+        'bard' => 'Бард',
+        'cleric' => 'Жрец',
+        'druid' => 'Друид',
+        'fighter' => 'Воин',
+        'monk' => 'Монах',
+        'paladin' => 'Паладин',
+        'ranger' => 'Следопыт',
+        'rogue' => 'Плут',
+        'sorcerer' => 'Чародей',
+        'warlock' => 'Колдун',
+        'wizard' => 'Волшебник',
+        'artificer' => 'Артифисер'
+    ];
+    
     public function __construct() {
         $this->dnd_api_service = new DndApiService();
         $this->ai_service = new AiService();
@@ -122,8 +166,8 @@ class CharacterGeneratorV4 {
             // Создаем персонажа
             $character = [
                 'name' => $this->generateName($race, $gender),
-                'race' => $race_data['name'],
-                'class' => $class_data['name'],
+                'race' => $this->getRaceDisplayName($race, $race_data),
+                'class' => $this->getClassDisplayName($class, $class_data),
                 'level' => $level,
                 'alignment' => $this->getAlignmentText($alignment),
                 'gender' => $this->getGenderText($gender),
@@ -137,9 +181,9 @@ class CharacterGeneratorV4 {
                 'attack_bonus' => $this->calculateAttackBonus($class_data, $abilities, $level),
                 'damage' => $this->calculateDamage($class_data, $abilities, $level),
                 'main_weapon' => $this->getMainWeapon($class_data),
-                'proficiencies' => $class_data['proficiencies'] ?? [],
-                'spells' => $spells,
-                'features' => $features,
+                'proficiencies' => $this->translateProficiencies($class_data['proficiencies'] ?? []),
+                'spells' => $this->processSpells($spells, $class_data, $level),
+                'features' => $this->processFeatures($features, $class_data, $level),
                 'equipment' => $equipment,
                 'saving_throws' => $this->getSavingThrows($class_data, $abilities),
                 'race_traits' => $race_data['traits'] ?? [],
@@ -413,9 +457,57 @@ class CharacterGeneratorV4 {
         }
         
         $ability_modifier = floor(($abilities[$primary_ability] - 10) / 2);
-        $proficiency_bonus = $this->calculateProficiencyBonus($level);
         
-        return $ability_modifier + $proficiency_bonus;
+        // Определяем базовый урон оружия в зависимости от уровня
+        $base_damage = $this->getBaseWeaponDamage($class_data['name'], $level);
+        
+        return $base_damage . '+' . $ability_modifier;
+    }
+    
+    /**
+     * Получение базового урона оружия по классу и уровню
+     */
+    private function getBaseWeaponDamage($class_name, $level) {
+        // Базовый урон для разных классов
+        $base_damage = [
+            'Варвар' => '1d12', // Топор
+            'Воин' => '1d8',    // Меч
+            'Плут' => '1d4',    // Кинжал
+            'Монах' => '1d6',   // Кулаки
+            'Паладин' => '1d8', // Меч
+            'Следопыт' => '1d8', // Лук
+            'Волшебник' => '1d6', // Посох
+            'Жрец' => '1d8',    // Булава
+            'Бард' => '1d8',    // Рапира
+            'Друид' => '1d6',   // Посох
+            'Чародей' => '1d6', // Посох
+            'Колдун' => '1d4',  // Кинжал
+            'Артифисер' => '1d8' // Молот
+        ];
+        
+        $damage = $base_damage[$class_name] ?? '1d6';
+        
+        // Увеличиваем урон с уровнем для воинских классов
+        if (in_array($class_name, ['Варвар', 'Воин', 'Паладин', 'Следопыт'])) {
+            if ($level >= 5) {
+                // Дополнительная атака на 5 уровне
+                $damage = '2' . substr($damage, 1);
+            }
+            if ($level >= 11) {
+                // Третья атака для Воина на 11 уровне
+                if ($class_name === 'Воин') {
+                    $damage = '3' . substr($damage, 1);
+                }
+            }
+            if ($level >= 20) {
+                // Четвертая атака для Воина на 20 уровне
+                if ($class_name === 'Воин') {
+                    $damage = '4' . substr($damage, 1);
+                }
+            }
+        }
+        
+        return $damage;
     }
     
     /**
@@ -446,12 +538,38 @@ class CharacterGeneratorV4 {
      */
     private function getSavingThrows($class_data, $abilities) {
         $saving_throws = [];
-        $proficiency_bonus = 2; // Для 1 уровня
+        $proficiency_bonus = $this->calculateProficiencyBonus(1); // Для 1 уровня
         
         if (isset($class_data['saving_throws'])) {
             foreach ($class_data['saving_throws'] as $ability) {
-                $ability_modifier = floor(($abilities[$ability] - 10) / 2);
-                $saving_throws[$ability] = $ability_modifier + $proficiency_bonus;
+                // Обрабатываем разные форматы названий характеристик
+                $ability_key = strtolower($ability);
+                if (isset($abilities[$ability_key])) {
+                    $ability_modifier = floor(($abilities[$ability_key] - 10) / 2);
+                    $saving_throws[$ability] = $ability_modifier + $proficiency_bonus;
+                } else {
+                    // Пробуем альтернативные названия
+                    $alt_names = [
+                        'str' => 'strength',
+                        'dex' => 'dexterity', 
+                        'con' => 'constitution',
+                        'int' => 'intelligence',
+                        'wis' => 'wisdom',
+                        'cha' => 'charisma'
+                    ];
+                    
+                    foreach ($alt_names as $short => $long) {
+                        if (isset($abilities[$short])) {
+                            $ability_modifier = floor(($abilities[$short] - 10) / 2);
+                            $saving_throws[$ability] = $ability_modifier + $proficiency_bonus;
+                            break;
+                        } elseif (isset($abilities[$long])) {
+                            $ability_modifier = floor(($abilities[$long] - 10) / 2);
+                            $saving_throws[$ability] = $ability_modifier + $proficiency_bonus;
+                            break;
+                        }
+                    }
+                }
             }
         }
         
@@ -558,6 +676,244 @@ class CharacterGeneratorV4 {
         $text = str_replace(["\xE2\x80\x98", "\xE2\x80\x99"], "'", $text);
         
         return trim($text);
+    }
+    
+    /**
+     * Получение отображаемого названия расы
+     */
+    private function getRaceDisplayName($race_key, $race_data) {
+        $race_key_lower = strtolower($race_key);
+        
+        // Сначала проверяем переводы
+        if (isset($this->race_translations[$race_key_lower])) {
+            return $this->race_translations[$race_key_lower];
+        }
+        
+        // Если нет перевода, используем данные из API
+        if (isset($race_data['name'])) {
+            return $race_data['name'];
+        }
+        
+        // Fallback на оригинальное название
+        return ucfirst($race_key);
+    }
+    
+    /**
+     * Получение отображаемого названия класса
+     */
+    private function getClassDisplayName($class_key, $class_data) {
+        $class_key_lower = strtolower($class_key);
+        
+        // Сначала проверяем переводы
+        if (isset($this->class_translations[$class_key_lower])) {
+            return $this->class_translations[$class_key_lower];
+        }
+        
+        // Если нет перевода, используем данные из API
+        if (isset($class_data['name'])) {
+            return $class_data['name'];
+        }
+        
+        // Fallback на оригинальное название
+        return ucfirst($class_key);
+    }
+    
+    /**
+     * Перевод владений на русский язык
+     */
+    private function translateProficiencies($proficiencies) {
+        $translations = [
+            'Light Armor' => 'Легкие доспехи',
+            'Medium Armor' => 'Средние доспехи',
+            'Heavy Armor' => 'Тяжелые доспехи',
+            'Shields' => 'Щиты',
+            'Simple Weapons' => 'Простое оружие',
+            'Martial Weapons' => 'Воинское оружие',
+            'Saving Throw: STR' => 'Спасбросок: СИЛ',
+            'Saving Throw: DEX' => 'Спасбросок: ЛОВ',
+            'Saving Throw: CON' => 'Спасбросок: ТЕЛ',
+            'Saving Throw: INT' => 'Спасбросок: ИНТ',
+            'Saving Throw: WIS' => 'Спасбросок: МДР',
+            'Saving Throw: CHA' => 'Спасбросок: ХАР'
+        ];
+        
+        $translated = [];
+        foreach ($proficiencies as $prof) {
+            $translated[] = $translations[$prof] ?? $prof;
+        }
+        
+        return $translated;
+    }
+    
+    /**
+     * Обработка заклинаний с учетом уровня персонажа
+     */
+    private function processSpells($spells, $class_data, $level) {
+        if (empty($spells) || !isset($class_data['spellcasting']) || !$class_data['spellcasting']) {
+            return [];
+        }
+        
+        // Определяем количество заклинаний по уровню
+        $spell_slots = $this->getSpellSlots($class_data['name'], $level);
+        
+        // Фильтруем заклинания по доступным уровням
+        $available_spells = [];
+        foreach ($spells as $spell) {
+            $spell_level = $spell['level'] ?? 0;
+            if ($spell_level <= $level) {
+                $available_spells[] = $spell;
+            }
+        }
+        
+        return [
+            'spells' => $available_spells,
+            'spell_slots' => $spell_slots,
+            'spellcasting_ability' => $class_data['spellcasting_ability'] ?? 'int'
+        ];
+    }
+    
+    /**
+     * Получение слотов заклинаний по классу и уровню
+     */
+    private function getSpellSlots($class_name, $level) {
+        $spell_slots = [];
+        
+        // Базовые слоты для полных заклинателей (Волшебник, Жрец, Друид, Бард, Чародей)
+        if (in_array($class_name, ['Волшебник', 'Жрец', 'Друид', 'Бард', 'Чародей'])) {
+            $spell_slots = [
+                1 => [4, 2, 0, 0, 0, 0, 0, 0, 0],
+                2 => [4, 3, 0, 0, 0, 0, 0, 0, 0],
+                3 => [4, 4, 2, 0, 0, 0, 0, 0, 0],
+                4 => [4, 4, 3, 0, 0, 0, 0, 0, 0],
+                5 => [4, 4, 3, 2, 0, 0, 0, 0, 0],
+                6 => [4, 4, 3, 3, 0, 0, 0, 0, 0],
+                7 => [4, 4, 3, 3, 1, 0, 0, 0, 0],
+                8 => [4, 4, 3, 3, 2, 0, 0, 0, 0],
+                9 => [4, 4, 3, 3, 3, 1, 0, 0, 0],
+                10 => [4, 4, 3, 3, 3, 2, 0, 0, 0],
+                11 => [4, 4, 3, 3, 3, 2, 1, 0, 0],
+                12 => [4, 4, 3, 3, 3, 2, 1, 0, 0],
+                13 => [4, 4, 3, 3, 3, 2, 1, 1, 0],
+                14 => [4, 4, 3, 3, 3, 2, 1, 1, 0],
+                15 => [4, 4, 3, 3, 3, 2, 1, 1, 1],
+                16 => [4, 4, 3, 3, 3, 2, 1, 1, 1],
+                17 => [4, 4, 3, 3, 3, 2, 1, 1, 1],
+                18 => [4, 4, 3, 3, 3, 2, 1, 1, 1],
+                19 => [4, 4, 3, 3, 3, 2, 1, 1, 1],
+                20 => [4, 4, 3, 3, 3, 2, 1, 1, 1]
+            ];
+        }
+        // Полузаклинатели (Паладин, Следопыт)
+        elseif (in_array($class_name, ['Паладин', 'Следопыт'])) {
+            $spell_slots = [
+                1 => [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                2 => [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                3 => [3, 0, 0, 0, 0, 0, 0, 0, 0],
+                4 => [3, 0, 0, 0, 0, 0, 0, 0, 0],
+                5 => [4, 2, 0, 0, 0, 0, 0, 0, 0],
+                6 => [4, 2, 0, 0, 0, 0, 0, 0, 0],
+                7 => [4, 3, 0, 0, 0, 0, 0, 0, 0],
+                8 => [4, 3, 0, 0, 0, 0, 0, 0, 0],
+                9 => [4, 3, 2, 0, 0, 0, 0, 0, 0],
+                10 => [4, 3, 2, 0, 0, 0, 0, 0, 0],
+                11 => [4, 3, 3, 0, 0, 0, 0, 0, 0],
+                12 => [4, 3, 3, 0, 0, 0, 0, 0, 0],
+                13 => [4, 3, 3, 1, 0, 0, 0, 0, 0],
+                14 => [4, 3, 3, 1, 0, 0, 0, 0, 0],
+                15 => [4, 3, 3, 2, 0, 0, 0, 0, 0],
+                16 => [4, 3, 3, 2, 0, 0, 0, 0, 0],
+                17 => [4, 3, 3, 3, 1, 0, 0, 0, 0],
+                18 => [4, 3, 3, 3, 1, 0, 0, 0, 0],
+                19 => [4, 3, 3, 3, 2, 0, 0, 0, 0],
+                20 => [4, 3, 3, 3, 2, 0, 0, 0, 0]
+            ];
+        }
+        // Третьезаклинатели (Бард, Колдун)
+        elseif (in_array($class_name, ['Бард', 'Колдун'])) {
+            $spell_slots = [
+                1 => [2, 0, 0, 0, 0, 0, 0, 0, 0],
+                2 => [3, 0, 0, 0, 0, 0, 0, 0, 0],
+                3 => [4, 2, 0, 0, 0, 0, 0, 0, 0],
+                4 => [4, 3, 0, 0, 0, 0, 0, 0, 0],
+                5 => [4, 3, 2, 0, 0, 0, 0, 0, 0],
+                6 => [4, 3, 3, 0, 0, 0, 0, 0, 0],
+                7 => [4, 3, 3, 1, 0, 0, 0, 0, 0],
+                8 => [4, 3, 3, 2, 0, 0, 0, 0, 0],
+                9 => [4, 3, 3, 3, 1, 0, 0, 0, 0],
+                10 => [4, 3, 3, 3, 2, 0, 0, 0, 0],
+                11 => [4, 3, 3, 3, 2, 1, 0, 0, 0],
+                12 => [4, 3, 3, 3, 2, 1, 0, 0, 0],
+                13 => [4, 3, 3, 3, 2, 1, 1, 0, 0],
+                14 => [4, 3, 3, 3, 2, 1, 1, 0, 0],
+                15 => [4, 3, 3, 3, 2, 1, 1, 1, 0],
+                16 => [4, 3, 3, 3, 2, 1, 1, 1, 0],
+                17 => [4, 3, 3, 3, 2, 1, 1, 1, 1],
+                18 => [4, 3, 3, 3, 2, 1, 1, 1, 1],
+                19 => [4, 3, 3, 3, 2, 1, 1, 1, 1],
+                20 => [4, 3, 3, 3, 2, 1, 1, 1, 1]
+            ];
+        }
+        
+        return $spell_slots[$level] ?? [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+    
+    /**
+     * Обработка способностей класса
+     */
+    private function processFeatures($features, $class_data, $level) {
+        if (empty($features)) {
+            // Генерируем базовые способности для класса
+            return $this->generateBasicFeatures($class_data['name'], $level);
+        }
+        
+        return $features;
+    }
+    
+    /**
+     * Генерация базовых способностей для класса
+     */
+    private function generateBasicFeatures($class_name, $level) {
+        $features = [];
+        
+        switch ($class_name) {
+            case 'Варвар':
+                if ($level >= 1) $features[] = ['name' => 'Ярость', 'description' => 'В бою вы можете впасть в ярость, получая преимущества в атаке и сопротивлении к урону.'];
+                if ($level >= 2) $features[] = ['name' => 'Безрассудная атака', 'description' => 'Вы можете атаковать безрассудно, получая преимущество, но давая врагам преимущество против вас.'];
+                if ($level >= 3) $features[] = ['name' => 'Путь варвара', 'description' => 'Вы выбираете путь, который формирует природу вашей ярости.'];
+                if ($level >= 5) $features[] = ['name' => 'Дополнительная атака', 'description' => 'Вы можете атаковать дважды вместо одного раза, когда используете действие Атака.'];
+                if ($level >= 7) $features[] = ['name' => 'Дикий инстинкт', 'description' => 'Вы получаете преимущество к инициативе.'];
+                if ($level >= 9) $features[] = ['name' => 'Брутальный критический удар', 'description' => 'Вы можете бросить дополнительную кость урона оружия при критическом ударе.'];
+                break;
+                
+            case 'Воин':
+                if ($level >= 1) $features[] = ['name' => 'Боевой стиль', 'description' => 'Вы выбираете стиль боя, который отражает вашу специализацию.'];
+                if ($level >= 2) $features[] = ['name' => 'Второе дыхание', 'description' => 'Вы можете использовать второе дыхание, чтобы восстановить здоровье.'];
+                if ($level >= 3) $features[] = ['name' => 'Боевой мастер', 'description' => 'Вы выбираете архетип, который отражает вашу специализацию.'];
+                if ($level >= 5) $features[] = ['name' => 'Дополнительная атака', 'description' => 'Вы можете атаковать дважды вместо одного раза, когда используете действие Атака.'];
+                if ($level >= 7) $features[] = ['name' => 'Боевой инстинкт', 'description' => 'Вы получаете преимущество к инициативе.'];
+                if ($level >= 9) $features[] = ['name' => 'Улучшенная критическая атака', 'description' => 'Вы можете бросить дополнительную кость урона оружия при критическом ударе.'];
+                break;
+                
+            case 'Плут':
+                if ($level >= 1) $features[] = ['name' => 'Скрытность', 'description' => 'Вы получаете бонус к проверкам Скрытности и Атлетики.'];
+                if ($level >= 2) $features[] = ['name' => 'Хитрость', 'description' => 'Вы можете использовать бонусное действие для выполнения определенных действий.'];
+                if ($level >= 3) $features[] = ['name' => 'Архетип плута', 'description' => 'Вы выбираете архетип, который отражает вашу специализацию.'];
+                if ($level >= 5) $features[] = ['name' => 'Неуловимость', 'description' => 'Вы получаете сопротивление к урону от заклинаний.'];
+                if ($level >= 7) $features[] = ['name' => 'Уклонение', 'description' => 'Вы можете использовать реакцию, чтобы уменьшить урон от атаки.'];
+                if ($level >= 9) $features[] = ['name' => 'Мастерство', 'features' => 'Вы получаете бонус к проверкам характеристик.'];
+                break;
+                
+            case 'Монах':
+                if ($level >= 1) $features[] = ['name' => 'Боевые искусства', 'description' => 'Вы можете использовать боевые искусства для увеличения урона.'];
+                if ($level >= 2) $features[] = ['name' => 'Ки', 'description' => 'Вы получаете доступ к ки-способностям.'];
+                if ($level >= 3) $features[] = ['name' => 'Путь монаха', 'description' => 'Вы выбираете путь, который формирует ваши способности.'];
+                if ($level >= 5) $features[] = ['name' => 'Дополнительная атака', 'description' => 'Вы можете атаковать дважды вместо одного раза, когда используете действие Атака.'];
+                if ($level >= 7) $features[] = ['name' => 'Уклонение', 'description' => 'Вы можете использовать реакцию, чтобы уменьшить урон от атаки.'];
+                if ($level >= 9) $features[] = ['name' => 'Улучшенное движение', 'description' => 'Вы получаете бонус к скорости.'];
+                break;
+        }
+        
+        return $features;
     }
 }
 
